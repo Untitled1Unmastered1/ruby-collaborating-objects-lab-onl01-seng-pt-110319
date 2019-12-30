88adaded75d1915fabd2926375152aa04433a31c
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2019-12-30 15:25:51 +0000

    Automatically backed up by Learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,17 @@
+require 'pry'
+class MP3Importer
+  attr_accessor :files, :path 
+  
+  def initialize(path)
+    @path = path
+    @files = Dir.entries(path).grep(/.*\.mp3/)
+  end
+  
+  def import
+    @files.each {|file| Song.new_by_filename(files)}
+  end 
+end 
+  
+  
+  
+  
